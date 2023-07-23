cf270fa9962b68f8eaadfb61daa61f67a807ad74
Maxim <34244452+m4ccim@users.noreply.github.com>
2023-07-23 14:57:03 +0300

    new input system

--- Assets/Scripts/UserControl.cs
+++ Assets/Scripts/UserControl.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using Buildings;
 using Units;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
@@ -14,25 +17,33 @@ public class UserControl : MonoBehaviour
     public Camera GameCamera;
     public float PanSpeed = 10.0f;
     public GameObject Marker;
-    
-    private Unit selected;
+
+    private List<Unit> selected;
 
     private float minZoom = 20;
     private float maxZoom = 3;
-    
-    
+
+    private PlayerInput playerInput;
+
     private void Start()
     {
-        Marker.SetActive(false);
+        //Marker.SetActive(false);
+        selected = new List<Unit>();
+        playerInput = new PlayerInput();
+        playerInput.Enable();
+        playerInput.Player.Select.performed += HandleSelection;
+        playerInput.Player.Order.performed += HandleOrder;
     }
-    public void HandleSelection()
+
+    public void HandleSelection(InputAction.CallbackContext callbackContext)
     {
         var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out var hit))
         {
+            selected.Clear();
             //the collider could be children of the unit, so we make sure to check in the parent
             var unit = hit.transform.GetComponentInParent<Unit>();
-            selected = unit;
+            selected.Add(unit);
 
 
             //check if the hit object have a IUIInfoContent to display in the UI
@@ -41,20 +52,24 @@ public class UserControl : MonoBehaviour
             UIMainScene.Instance.SetNewInfoContent(uiInfo);
         }
     }
-    private void HandleAction()
+
+    private void HandleOrder(InputAction.CallbackContext callbackContext)
     {
+        if(!selected.Any()) return;
         var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             var building = hit.collider.GetComponentInParent<Building>();
-
-            if (building != null)
+            foreach (var selectedUnit in selected)
             {
-                selected.GoTo(building);
-            }
-            else
-            {
-                selected.GoTo(hit.point);
+                if (building != null)
+                {
+                    selectedUnit.GoTo(building);
+                }
+                else
+                {
+                    selectedUnit.GoTo(hit.point);
+                }
             }
         }
     }
@@ -65,30 +80,23 @@ public class UserControl : MonoBehaviour
         Vector2 move = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         gameCameraTransform.position += PanSpeed * Time.deltaTime * new Vector3(move.y, 0, -move.x);
         Vector2 zoom = Input.mouseScrollDelta;
-        
+
         if ((GameCamera.transform.position.y > maxZoom || zoom.y < 0) &&
             (GameCamera.transform.position.y < minZoom || zoom.y > 0))
         {
             gameCameraTransform.position += gameCameraTransform.forward * (zoom.y * (PanSpeed * 10 * Time.deltaTime));
         }
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            HandleSelection();
-        }
-        else if (selected != null && Input.GetMouseButtonDown(1))
-        {//right click give order to the unit
-            HandleAction();
-        }
+        
 
         MarkerHandling();
     }
+    
+    
 
     // Handle displaying the marker above the unit that is currently selected (or hiding it if no unit is selected)
     void MarkerHandling()
     {
-        if (selected == null && Marker.activeInHierarchy)
+        /*if (selected == null && Marker.activeInHierarchy)
         {
             Marker.SetActive(false);
             Marker.transform.SetParent(null);
@@ -98,6 +106,6 @@ public class UserControl : MonoBehaviour
             Marker.SetActive(true);
             Marker.transform.SetParent(selected.transform, false);
             Marker.transform.localPosition = Vector3.zero;
-        }    
+        }  */
     }
 }
